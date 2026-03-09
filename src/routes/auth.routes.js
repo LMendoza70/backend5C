454af474f30be74d58141664d710c6df8087d999
@LMendoza70 <luis.mendoza@uthh.edu.mx>
2026-03-09 08:55:49 -0600

    fix: correct export statement for verificarToken and update auth controller import path (areglamos nuestro desmadre)

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import * as authCtrl from '../controllers/auth.controller.js';
+import * as authCtrl from '../controllers/usuarios.controller.js';
 
 const router = Router();
 
